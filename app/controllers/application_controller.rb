6ccde93fd6b9f49ef0a92c1f0bb079d7270e9aa4
Louis Lloyd-Besson <louislb@eante.com.au>
2019-07-23 14:38:03 +1000

    Added ability to edit logged it devise users details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
 
           def configure_permitted_parameters
                devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :surname, :contact_number, :address, :email, :password)}
+               devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :surname, :contact_number, :address, :email, :password, :current_password)}
           end
 
 
