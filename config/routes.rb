e758d0d497031e9200d747a2a3b34a87041f4da7
vivecuervo7 <vivecuervo7@gmail.com>
2019-07-31 14:09:46 +1000

    nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   get 'payments/new'
   get 'services/my_services', to: 'services#my_services', as: 'my_services'
+  get 'proposals', to: 'proposals#index', as: 'proposals'
   devise_for :users
   root to: 'home#index'
-  resources :services
-  resources :proposals, only: [:index, :new, :create]
+  resources :services do
+    resources :proposals, only: [:new, :create, :edit, :update] do
+      resources :payments, only: [:new]
+    end
+  end
 end
