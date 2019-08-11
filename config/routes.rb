5b86d5ef6b665e4ca02574e5c80dcd93838fa7c3
Louis Lloyd-Besson <louislb@eante.com.au>
2019-08-12 09:35:08 +1000

    fixed stripe

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'services/:id/mark_complete', to: 'services#complete_service', as: 'complete_service'
   patch 'services/:service_id/proposals/:id/accept', to: 'services#accept_proposal', as: 'accept_proposal'
   get 'proposals', to: 'proposals#index', as: 'proposals'
-  get 'services/:service_id/proposals/:proposal_id/payment/success', to: 'services#job_paid'
+  get 'services/:service_id/proposals/:proposal_id/payment/success', to: 'services#job_paid', as: 'payment_success'
   patch 'users/:user_id/add_review', to: 'users#add_review', as: 'add_review'
   devise_for :users
   root to: 'home#index'
